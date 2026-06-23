8d59417dbda09a9b2c2bbd5b3a6e49fdeeb19b99
Justin Adams <AdamsJustin@protonmail.com>
2026-06-22 20:57:02 -0400

    Added ebitengine UI

--- main.go
+++ main.go
@@ -3,33 +3,55 @@ package main
 import (
 	"fmt"
 	"gopher8/internal/core"
+	"image/color"
 	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
 )
+
+type Game struct {
+	cpu core.CPU
+}
+
+func (g *Game) Update() error {
+	g.cpu.Cycle()
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	for r := range core.Height {
+		for c := range core.Width {
+			if g.cpu.Display[r][c] == true {
+				screen.Set(c, r, color.White)
+			} else {
+				screen.Set(c, r, color.Black)
+			}
+		}
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return core.Width, core.Height
+}
 
 func main() {
 	fmt.Println("Go Chip8")
 
-	rom, err := core.CreateROM("roms/IBM Logo.ch8")
+	rom, err := core.CreateROM("roms/test_opcode.ch8")
 	if err != nil {
 		log.Fatal(err)
 	}
 	cpu := core.NewCPU()
 	cpu.LoadBytes(core.RomStart, rom.Data[:])
 
-	for {
-		cpu.Cycle()
+	game := &Game{
+		cpu: *cpu,
+	}
 
-		// draw to terminal
-		for r := range core.Height {
-			for c := range core.Width {
-				if cpu.Display[r][c] == true {
-					fmt.Print(" *")
-				} else {
-					fmt.Print("  ")
-				}
-			}
-			fmt.Println()
-		}
+	ebiten.SetWindowSize(core.Width*10, core.Height*10)
+	ebiten.SetWindowTitle("Gopher8")
 
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
 	}
 }
